c67089b7b2310c206639ada97f316e1168579642
Nisratul khaira <nisratulkhaira1@gmail.com>
2026-07-06 16:00:10 +0700

    memperbaiki file koneksi

--- koneksi.php
+++ koneksi.php
@@ -1,9 +1,8 @@
 <?php
-<<<<<<< HEAD
-$conn = mysqli_connect("localhost", "root", "", "db_klinik_kampus.sql");
-=======
+
+
 $conn = mysqli_connect("localhost", "root", "", "db_klinik_kampus");
->>>>>>> f9e3b5909b1dbd1396f6a3c4ea777301efc354cc
+
 
 if (!$conn) {
     die("Koneksi database gagal: " . mysqli_connect_error());
